4d8ec4e29b86950a0c68051a12a6e88a76add457
Ziad Khobeiz <ziadkhobeiz@hotmail.com>
2022-11-07 01:50:55 +0200

    Add Quantization and Encoding

--- DSPComponents/Algorithms/QuantizationAndEncoding.cs
+++ DSPComponents/Algorithms/QuantizationAndEncoding.cs
@@ -21,7 +21,63 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
-            throw new NotImplementedException();
+
+            if (InputLevel <= 0)
+                InputLevel = (1 << InputNumBits);
+
+            else
+                InputNumBits = getNumberOfBits(InputLevel - 1);
+
+            float minSample = InputSignal.Samples.Min();
+            float maxSample = InputSignal.Samples.Max();
+            float delta = (maxSample - minSample) / InputLevel;
+            int numberOfSamples = InputSignal.Samples.Count;
+
+            OutputQuantizedSignal = new Signal(new List<float>(numberOfSamples), false);
+            OutputIntervalIndices = new List<int>(numberOfSamples);
+            OutputEncodedSignal = new List<string>(numberOfSamples);
+            OutputSamplesError = new List<float>(numberOfSamples);
+
+
+            foreach(float sample in InputSignal.Samples)
+            {
+                int index = (int)((sample - minSample) / delta);
+                if (index == InputLevel) index = InputLevel - 1;
+
+                OutputIntervalIndices.Add(index + 1);
+
+                var encodedSample = toBinary(index, InputNumBits);
+                OutputEncodedSignal.Add(encodedSample);
+
+                OutputQuantizedSignal.Samples.Add(minSample + (index * delta) + (delta / 2));
+
+                float error = OutputQuantizedSignal.Samples.Last() - sample;
+                OutputSamplesError.Add(error);
+            }
+        }
+        private String toBinary(int x, int numberOfBits)
+        {
+            String binaryNumber = "";
+
+            for(int i = 0; i < numberOfBits; i++)
+            {
+                binaryNumber += (x % 2 == 1 ? '1' : '0');
+                x /= 2;
+            }
+            char[] reversedBinaryNumberArray = binaryNumber.ToCharArray();
+            Array.Reverse(reversedBinaryNumberArray);
+            return new string(reversedBinaryNumberArray);
+        }
+
+        private int getNumberOfBits(int x)
+        {
+            int numberOfBits = 0;
+            while(x > 0)
+            {
+                numberOfBits++;
+                x /= 2;
+            }
+            return numberOfBits;
         }
     }
 }
